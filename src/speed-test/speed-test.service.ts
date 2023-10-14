36f6eaecb1aa44794049ba6fa1092f92aa6543e1
Robert Anton <mr.robert.anton@gmail.com>
2023-10-14 16:32:53 +0200

    adds formatting

--- src/speed-test/speed-test.service.ts
+++ src/speed-test/speed-test.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 import * as path from 'path';
 import * as childProcess from 'child_process';
 import { chMod } from './utils/chmod.util';
-import { formatLine } from './utils/format-line.util';
-import * as kill from 'tree-kill';
+import { fromEvent } from 'rxjs';
+import * as stringDecoder from 'string_decoder';
 
 @Injectable()
 export class SpeedTestService {
@@ -30,168 +30,51 @@ export class SpeedTestService {
   async executeSpeedTest() {
     try {
       const binary = await this.ensureBinary();
-      const args = ['-p', '--accept-license', '--accept-gdpr'];
+      const args = ['-p', '--accept-license', '--accept-gdpr', '--format=json'];
+
+      const decoder = new stringDecoder.StringDecoder('utf8');
 
       const cliProcess = childProcess.spawn(binary, args);
-      cliProcess.on('error', (err) => {
-        console.error(err);
+
+      cliProcess.stdout.on('data', (data) => {
+        try {
+          const stringData = decoder.write(data);
+          const lines = stringData?.trim().split('\n');
+
+          lines.forEach((line) => {
+            const json = JSON.parse(line);
+            console.log(json);
+          });
+          // const json = JSON.parse(stringData);
+          console.log(data);
+        } catch (e) {
+          console.error(e);
+        }
       });
 
-      function pendingPromise() {
-        let resolve = undefined;
-        let reject = undefined;
-        const promise = new Promise((res, rej) => {
-          resolve = res;
-          reject = rej;
-        });
-        return { promise, resolve, reject };
-      }
+      const obsFrom = fromEvent(cliProcess.stdout, 'data');
+      const obsError = fromEvent(cliProcess.stderr, 'data');
+      const obsClose = fromEvent(cliProcess, 'close');
+      const obsExit = fromEvent(cliProcess, 'exit');
 
-      const { promise, resolve, reject: rejectPromise } = pendingPromise();
+      obsClose.subscribe((data) => {
+        console.log('close', data);
+      });
 
-      const reject = (err) => {
-        aborted = true;
-        rejectPromise(err);
-      };
+      obsExit.subscribe((data) => {
+        console.log('exit', data);
+      });
 
-      const handleStdout = (isError, line) => {
-        if (aborted) return;
-        if (/^{/.test(line)) {
-          let data;
-          try {
-            data = JSON.parse(line);
-          } catch (err) {
-            // Ignore
-          }
-          if (data) {
-            if (data.timestamp) {
-              data.timestamp = new Date(data.timestamp);
-            }
-            if (data.type) {
-              const content = data[data.type];
-              if (content) {
-                if (currentPhase !== data.type && progressPhases[data.type]) {
-                  priorProgress += progressPhases[currentPhase] || 0;
-                  currentPhase = data.type;
-                }
-                if (
-                  typeof content.progress === 'number' &&
-                  progressPhases[data.type]
-                ) {
-                  data.progress =
-                    priorProgress +
-                    progressPhases[data.type] * content.progress;
-                }
-              }
-            } else {
-              if (data.suite || data.app || data.servers) {
-                data.type = 'config';
-              }
-            }
-            if (data.progress === undefined) {
-              data.progress = priorProgress;
-            }
-            lastProgress = data.progress = Math.max(
-              data.progress,
-              lastProgress,
-            );
-            if (data.error) {
-              return reject(new Error(data.error));
-            }
-            if (data.type === 'log' && data.level === 'error') {
-              return reject(new Error(data.message));
-            }
-            if (data.type === 'result') {
-              delete data.progress;
-              delete data.type;
-              result = data;
-              return;
-            }
-            // if (progress) {
-            //   progress(data);
-            // }
-            return;
-          }
-        }
-        if (!line.trim()) return;
-        if (isError) {
-          if (!/] \[(info|warning)]/.test(line)) {
-            errorLines.push(line);
-          }
-        }
-      };
-      // if (
-      //   // cancel(setCancelHandler, () => {
-      //   //   aborted = true;
-      //   //   process.nextTick(() => reject(new Error('Test aborted')));
-      //   // })
-      // ) {
-      //   throw new Error('Test aborted');
-      // }
-
-      const progressPhases = {
-        ping: 2,
-        download: 15,
-        upload: 6,
-      };
-      const totalTime = Object.keys(progressPhases).reduce(
-        (total, key) => total + progressPhases[key],
-        0,
-      );
-      Object.keys(progressPhases).forEach(
-        (key) => (progressPhases[key] /= totalTime),
-      );
-
-      const errorLines = [];
-      let priorProgress = 0;
-      let lastProgress = 0;
-      let aborted = false;
-      let result = undefined;
-      let currentPhase;
-
-      formatLine(cliProcess.stdout, handleStdout.bind(null, false));
-      formatLine(cliProcess.stderr, handleStdout.bind(null, true));
-
-      cliProcess.on('exit', resolve);
-      cliProcess.on('error', reject);
-
-      try {
-        await promise;
-      } finally {
-        const pid = cliProcess.pid;
-        cliProcess.kill();
-        kill(pid);
-      }
-      if (errorLines.length) {
-        const licenseAcceptedMessage =
-          /License acceptance recorded. Continuing./;
-        const acceptLicenseMessage =
-          /To accept the message please run speedtest interactively or use the following:[\s\S]*speedtest --accept-license/;
-        const acceptGdprMessage =
-          /To accept the message please run speedtest interactively or use the following:[\s\S]*speedtest --accept-gdpr/;
-
-        let error = errorLines.join('\n');
-
-        if (licenseAcceptedMessage.test(error)) {
-          error = '';
-        } else if (acceptLicenseMessage.test(error)) {
-          error = error.replace(
-            acceptLicenseMessage,
-            'To accept the message, pass the acceptLicense: true option',
-          );
-        } else if (acceptGdprMessage.test(error)) {
-          error = error.replace(
-            acceptGdprMessage,
-            'To accept the message, pass the acceptGdpr: true option',
-          );
-        } else {
-          error = error.replace(/===*[\s\S]*about\/privacy\n?/, '');
-        }
-        error = error.trim();
-        if (error) throw new Error(error);
-      }
-      aborted = true;
-      return result;
+      // obsFrom.subscribe((data: Buffer) => {
+      //   try {
+      //     const stringData = decoder.write(data);
+      //     const lines = stringData?.trim().split('\n');
+      //
+      //     console.log(stringData);
+      //   } catch (e) {
+      //     console.error(e);
+      //   }
+      // });
     } catch (e) {
       console.error(e);
     }
